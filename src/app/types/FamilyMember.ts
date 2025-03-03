34594089938e99b5efda70ff8e1d57e1047a71d9
Syed Mohammad Raza Rizvi <asimr6573@gmail.com>
2025-03-03 21:26:42 +0500

    first commit

--- src/app/types/FamilyMember.ts
+++ src/app/types/FamilyMember.ts
@@ -1,19 +1,18 @@
 export interface FamilyMember {
-  reasonOfDeath: any
-  placeOfBurial: string
-  id: string
-  name: string
-  birthDate: string
-  deathDate?: string
-  generationNo: number
-  image: string
-  parentId: string
-  spouseId: string
-  siblings: string[]
-  children: string[]
-  gender: "male" | "female" | "other"
-  occupation: string
-  education: string
-  notes: string
+  reasonOfDeath: string | null; // Changed from `any` to `string | null`
+  placeOfBurial: string;
+  id: string;
+  name: string;
+  birthDate: string;
+  deathDate?: string;
+  generationNo: number;
+  image: string;
+  parentId: string;
+  spouseId: string;
+  siblings: string[];
+  children: string[];
+  gender: "male" | "female" | "other";
+  occupation: string;
+  education: string;
+  notes: string;
 }
-
